4869b3de2798da70b637800256898a969359e029
nagatapote <hiroaki29p@icloud.com>
2021-10-23 22:35:23 +0900

    feat: rss

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -6,6 +6,29 @@ import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
+export function getAllPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData
+}
+
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
@@ -19,7 +42,6 @@ export function getSortedPostsData() {
 
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
-
     // Combine the data with the id
     return {
       id,
---
+++ pages/rss.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from 'next';
+import Rss from 'rss';
+import { getAllPostsData } from '../lib/posts';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { res } = ctx;
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/rss+xml;charset=utf-8');
+
+  const allPostsData = getAllPostsData();
+
+  const rss = new Rss({
+    title: 'Moi',
+    site_url: "https://nagatapote.work/",
+    feed_url: `https://nagatapote.work/rss`,
+    language: 'ja',
+    description:
+      '自分の好きなことをアウトプットするブログです。',
+    copyright: '©︎Moi',
+  });
+
+  type Post = {
+    id: string,
+    title: string,
+    date: string,
+    image: string,
+    profile: string,
+    user: string,
+    category: string,
+}
+
+  allPostsData.forEach((post:Post) => {
+    rss.item({
+      title: post.title,
+      url: `https://nagatapote.work/${post.id}`,
+      description: post.category,
+      date: new Date(post.date),
+    });
+  });
+
+  res.end(rss.xml());
+
+  return { props: {} };
+};
+
+export default () => {};
